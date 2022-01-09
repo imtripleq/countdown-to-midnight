a21f123d1a55e3384562543a8c53357e6ddd76e4
imtripleq <flamework89@hotmail.com>
2022-01-09 11:50:15 +1100

    done

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,25 @@
 #write your code here
 
+
 def countdown
+    x= 10
+    while x > 0
+        puts "#{x} seconds"
+        x -= 1
+    end
+    puts "Happy New Year!"
 end
+
+
+
+def countdown_with_sleep
+    x = 10
+    while x > 0
+        puts "#{x} seconds"
+        x -= 1
+        sleep(1)
+    end
+    puts "Happy New Year!"
+end
+
+countdown_with_sleep
